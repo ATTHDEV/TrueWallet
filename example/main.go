2dc033aba0b3a84b639985f475149f5e924b9901
ATTH <atthawutDev@gmail.com>
2018-12-07 18:18:39 +0700

    add example

---
+++ example/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ATTHDEV/TruwWallet"
+)
+
+func main() {
+	wallet := TrueWallet.New("xxxxxxxxxx", "xxxx") //put you mobile number and pin
+
+	fmt.Println("this is last 50 transaction for today")
+
+	transaction := wallet.GetTransaction()
+	fmt.Println(transaction)
+}
